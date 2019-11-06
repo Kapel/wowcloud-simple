68664021b98e355b19e4ff2b4d8a52f953ee1da4
kapel <26456185+Kapel@users.noreply.github.com>
2019-11-06 23:51:16 +0100

    fix missing route table

--- terraform/route-tables.tf
+++ terraform/route-tables.tf
@@ -6,7 +6,13 @@ resource "aws_route_table" "PUBLIC" {
     gateway_id = "${aws_internet_gateway.WOWCLOUD-InternetGateway.id}"
   }
 
+
   tags = {
     Name = "WOWCLOUD-public-route-table"
   }
+}
+
+resource "aws_route_table_association" "wowcloud-simple" {
+  subnet_id      = "${aws_subnet.WOWCLOUD-EC2.id}"
+  route_table_id = "${aws_route_table.PUBLIC.id}"
 }
